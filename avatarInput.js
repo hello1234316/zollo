97d47a5694152281e8693e6fcc31ccc2f8e152e7
HELLO Phone <85028651+hello1234316@users.noreply.github.com>
2021-11-03 17:23:53 +0800

    Create  avatarInput.js

---
+++ avatarInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class AvatarInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { check: props.default };
+  }
+
+  handleChange(e) {
+    this.setState({ check: e.target.value });
+    this.props.onAvatarChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <fieldset>
+        <legend>頭像</legend>
+        <input type="radio" name="avatar" id="new" value="sena" onChange={this.handleChange} checked={this.state.check === "sena"} />
+        <label htmlFor="sena">原始</label>
+        <input type="radio" name="avatar" id="old" value="diep" onChange={this.handleChange} checked={this.state.check === "diep"} />
+        <label htmlFor="diep"></label>
+      </fieldset>
+    )
+  }
+}
+
+export default AvatarInput;
